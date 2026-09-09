e911742690a41703a62cb108a129812243f44a8c
Kalyan Babu Adda <kalyanbabuadda40@gmail.com>
2026-09-09 06:05:28 +0530

    Create products.js

---
+++ products.js
@@ -0,0 +1,56 @@
+const products = [
+
+    {
+
+        id: 1,
+
+        number: "01",
+
+        name: "PAWAN KALYAN",
+
+        image: "images/product-1.png",
+
+        price: 999,
+
+        description:
+            "Oversized premium streetwear tee featuring a bold PAWAN KALYAN design.",
+
+    },
+
+
+    {
+
+        id: 2,
+
+        number: "02",
+
+        name: "POWER STAR",
+
+        image: "images/product-2.png",
+
+        price: 999,
+
+        description:
+            "Minimal front design combined with a powerful graphic statement on the back.",
+
+    },
+
+
+    {
+
+        id: 3,
+
+        number: "03",
+
+        name: "KALYAN BABU",
+
+        image: "images/product-3.png",
+
+        price: 999,
+
+        description:
+            "Bold typography and a powerful streetwear graphic created for the true fans.",
+
+    }
+
+];
